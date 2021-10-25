d012a8eb3ab9edc4abf795d7da16242f9074a10a
Man <tranman731@gmail.com>
2021-10-25 14:42:29 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,35 @@
+package basic.dev;
+/**
+ * 
+ * @author Acer
+ *
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		/**
+		 * Khai báo biến
+		 */
+		String firstName = "Tran";
+		String lastName = "Man";
+		int old = 18;
+		float markMath = 9f ;
+		boolean passed = true ;
+		
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + "" + lastName );
+		System.out.println("Mark Math : " + markMath );
+		System.out.println("Passed: " + passed );
+		// Khai báo hằng . Hằng phải viết hoa
+		final float PI =3.14f;
+		
+		// TODO Auto-generated method stub
+		// day la bien cuc bo
+		
+	
+		
+
+	}
+
+}
